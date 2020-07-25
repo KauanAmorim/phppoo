4a048e91afcc47eaceb25b4a4cf739da9f122242
KauanAmorim <kauanamorim88@gmail.com>
2020-07-24 22:51:15 -0300

    estudando encapsulamento com active record layer supertype

--- classes/model/Produto.php
+++ classes/model/Produto.php
@@ -7,4 +7,13 @@ use \api\Record;
 class Produto extends Record
 {
     const TABLENAME = 'produto';
+
+    public function set_estoque($estoque)
+    {
+        if(\is_numeric($estoque) && $estoque > 0){
+            $this->data['estoque'] = $estoque;
+        } else {
+            throw new \Exception("Estoque {$estoque} inválido em ". __CLASS__);
+        }
+    }
 }
